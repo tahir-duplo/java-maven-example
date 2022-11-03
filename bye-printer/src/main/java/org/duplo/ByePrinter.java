ca4e731d6393ecc82702f47162c87f865263cfb1
tahir-duplo <tahir@duplocloud.net>
2022-11-03 16:23:22 +0530

    hello and bye printer example

---
+++ bye-printer/src/main/java/org/duplo/ByePrinter.java
@@ -0,0 +1,7 @@
+package org.duplo;
+
+public class ByePrinter {
+	public static void byePrint(String param) {
+		System.out.println("Bye " + param);
+	}
+}
---
+++ hello-printer/src/main/java/org/dulo/HelloPrinter.java
@@ -0,0 +1,7 @@
+package org.dulo;
+
+public class HelloPrinter {
+	public static void helloPrint(String param) {
+		System.out.println("Hello " + param);
+	}
+}
